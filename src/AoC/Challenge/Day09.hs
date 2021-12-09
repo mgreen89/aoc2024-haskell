bf2b2344db2a2a7331977898ea50a62ce6c4973b
Matthew Green <mgreen89@gmail.com>
2021-12-09 18:12:25 +0000

    Day 9 part b

--- src/AoC/Challenge/Day09.hs
+++ src/AoC/Challenge/Day09.hs
@@ -1,14 +1,17 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-
 module AoC.Challenge.Day09
   ( day09a
   , day09b
   ) where
 
 import           AoC.Solution
+import           Control.Monad                  ( mfilter )
+import           Data.Foldable                  ( toList )
+import           Data.List                      ( sort )
 import           Data.Map                       ( Map )
 import qualified Data.Map                      as M
+import           Data.Maybe                     ( listToMaybe
+                                                , mapMaybe
+                                                )
 import           Linear.V2                      ( V2(..) )
 import           Text.Read                      ( readEither )
 
@@ -44,5 +47,38 @@ day09a = Solution { sParse = parseMap
                   , sSolve = Right . sum . fmap ((+ 1) . snd) . getLows
                   }
 
-day09b :: Solution _ _
-day09b = Solution { sParse = Right, sShow = show, sSolve = Right }
+-- Map from each point to a lower neighbour, or Nothing if none (or 9, since
+-- don't want to count those in part b).
+slopeMap :: Map Point Int -> Map Point (Maybe Point)
+slopeMap m = M.mapWithKey go m
+ where
+  go p v = if v > 8
+    then Nothing
+    else listToMaybe . mapMaybe filt $ getNeighbours p
+   where
+    -- If the point has a height of less than p, get the point
+    -- (and not the height!)
+    filt :: Point -> Maybe Point
+    filt q = q <$ mfilter (< v) (M.lookup q m)
+
+-- | Create a map of elem -> frequency.
+getFreqs :: (Foldable f, Ord a) => f a -> Map a Int
+getFreqs = M.fromListWith (+) . map (, 1) . toList
+
+-- Create map of basin lowest-point to basin size.
+getBasins :: Map Point Int -> Map Point Int
+getBasins m =
+  let pointToBasinFloor :: Map Point Point
+      pointToBasinFloor = M.mapWithKey go (slopeMap m)
+      go :: Point -> Maybe Point -> Point
+      go p l = case l of
+        Just q  -> pointToBasinFloor M.! q
+        Nothing -> p
+  in  getFreqs pointToBasinFloor
+
+day09b :: Solution (Map Point Int) Int
+day09b = Solution
+  { sParse = parseMap
+  , sShow  = show
+  , sSolve = Right . product . take 3 . reverse . sort . M.elems . getBasins
+  }
