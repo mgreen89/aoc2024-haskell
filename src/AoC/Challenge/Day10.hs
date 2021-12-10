3846f523ad1d19e8608a9503eb1d61a66a83709c
Matthew Green <mgreen89@gmail.com>
2021-12-10 10:54:16 +0000

    Tidy up Day 10 a little

--- src/AoC/Challenge/Day10.hs
+++ src/AoC/Challenge/Day10.hs
@@ -4,16 +4,18 @@ module AoC.Challenge.Day10
   ) where
 
 import           AoC.Solution
+import           Data.Either                    ( lefts
+                                                , rights
+                                                )
 import           Data.Foldable                  ( foldl' )
 import           Data.List                      ( sort )
-import           Data.Maybe                     ( mapMaybe )
 
-parenToScore :: Char -> Int
-parenToScore ')' = 3
-parenToScore ']' = 57
-parenToScore '}' = 1197
-parenToScore '>' = 25137
-parenToScore x   = error $ "Invalid paren score: " <> pure x
+parenCorruptScore :: Char -> Int
+parenCorruptScore ')' = 3
+parenCorruptScore ']' = 57
+parenCorruptScore '}' = 1197
+parenCorruptScore '>' = 25137
+parenCorruptScore x   = error $ "Invalid paren corrupt score: " <> pure x
 
 other :: Char -> Char
 other '(' = ')'
@@ -34,19 +36,20 @@ checkChar stack c = if isOpen c
     _                         -> Left c
   where isOpen = flip elem ['(', '[', '{', '<']
 
-checkLine :: String -> Maybe Int
+-- Return Either (Left corruptScore) (Right stack)
+checkLine :: String -> Either Int [Char]
 checkLine = go []
  where
   go stack cs = case cs of
     (c : rest) -> case checkChar stack c of
-      Left  i   -> Just $ parenToScore i
+      Left  i   -> Left $ parenCorruptScore i
       Right ci' -> go ci' rest
-    _ -> Nothing
+    _ -> Right stack
 
 day10a :: Solution [String] Int
 day10a = Solution { sParse = Right . lines
                   , sShow  = show
-                  , sSolve = Right . sum . mapMaybe checkLine
+                  , sSolve = Right . sum . lefts . fmap checkLine
                   }
 
 parenCompleteScore :: Char -> Int
@@ -54,17 +57,12 @@ parenCompleteScore ')' = 1
 parenCompleteScore ']' = 2
 parenCompleteScore '}' = 3
 parenCompleteScore '>' = 4
-parenCompleteScore x   = error $ "Invalid paren: " <> pure x
+parenCompleteScore x   = error $ "Invalid paren complete score: " <> pure x
 
-completeLine :: String -> Maybe Int
-completeLine = fmap complete . check []
+-- Return Either (Left corruptScore) (Right completeScore)
+completeLine :: String -> Either Int Int
+completeLine = fmap complete . checkLine
  where
-  check stack cs = case cs of
-    (c : rest) -> case checkChar stack c of
-      Left  _   -> Nothing
-      Right ci' -> check ci' rest
-    _ -> Just stack
-
   complete = foldl' go 0
     where go tot x = (tot * 5) + parenCompleteScore (other x)
 
@@ -72,12 +70,12 @@ completeLine = fmap complete . check []
 -- This assumes there's an odd number in the list!
 median :: [Int] -> Int
 median xs =
-  let s      = sort xs
-      l      = length s
+  let s = sort xs
+      l = length s
   in  (s !! (l `div` 2))
 
 day10b :: Solution [String] Int
 day10b = Solution { sParse = Right . lines
                   , sShow  = show
-                  , sSolve = Right . median . mapMaybe completeLine
+                  , sSolve = Right . median . rights . fmap completeLine
                   }
