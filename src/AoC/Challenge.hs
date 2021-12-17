2586a471bdc9434e25b5c475a38380c6f4f7682d
Matthew Green <mgreen89@gmail.com>
2021-12-17 11:27:27 +0000

    Day 17 part b

--- src/AoC/Challenge.hs
+++ src/AoC/Challenge.hs
@@ -110,7 +110,7 @@ solutionList =
   , (mkDay_ 16, (Part1, SomeSolution day16a))
   , (mkDay_ 16, (Part2, SomeSolution day16b))
   , (mkDay_ 17, (Part1, SomeSolution day17a))
-  --, (mkDay_ 17, (Part2, SomeSolution day17b))
+  , (mkDay_ 17, (Part2, SomeSolution day17b))
   ]
 
 -- | Get a map of the completed solution parts for the given day.
--- src/AoC/Challenge/Day17.hs
+++ src/AoC/Challenge/Day17.hs
@@ -1,21 +1,19 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-
 module AoC.Challenge.Day17
   ( day17a
-  -- , day17b
+  , day17b
   ) where
 
 import           AoC.Solution
+import           AoC.Util                       ( Point )
 import           Data.Bifunctor                 ( first )
 import           Data.Void                      ( Void )
+import           Linear.V2                      ( V2(..) )
+import           Linear.Vector                  ( zero )
 import qualified Text.Megaparsec               as MP
 import qualified Text.Megaparsec.Char          as MP
 import qualified Text.Megaparsec.Char.Lexer    as MPL
 
-parser :: MP.Parsec Void String ((Int, Int), (Int, Int))
+parser :: MP.Parsec Void String (V2 Point)
 parser = do
   MP.string "target area: x="
   xMin <- signedInt
@@ -25,10 +23,10 @@ parser = do
   yMin <- signedInt
   MP.string ".."
   yMax <- signedInt
-  pure ((xMin, xMax), (yMin, yMax))
+  pure $ V2 (V2 xMin yMin) (V2 xMax yMax)
   where signedInt = MPL.signed MP.space MPL.decimal
 
-parse :: String -> Either String ((Int, Int), (Int, Int))
+parse :: String -> Either String (V2 Point)
 parse = first MP.errorBundlePretty . MP.parse parser "day17"
 
 {- This is pretty trivial without writing a program, but here
@@ -47,15 +45,50 @@ parse = first MP.errorBundlePretty . MP.parse parser "day17"
    it's below zero, need the (n-1)th as (n-1) will be the final step
    above y=0.
 -}
-partA :: (Int, Int) -> Int
-partA (yMin, yMax) =
+partA :: V2 Point -> Int
+partA (V2 (V2 _ yMin) (V2 _ yMax)) =
   let y = if abs yMin > abs yMax then yMin else yMax
       tri x = (x * (x + 1)) `div` 2
   in  if y > 0 then tri y else tri (abs y - 1)
 
-day17a :: Solution ((Int, Int), (Int, Int)) Int
-day17a =
-  Solution { sParse = parse, sShow = show, sSolve = Right . partA . snd }
+day17a :: Solution (V2 Point) Int
+day17a = Solution { sParse = parse, sShow = show, sSolve = Right . partA }
 
-day17b :: Solution _ _
-day17b = Solution { sParse = parse, sShow = show, sSolve = Right }
+-- Step the position and velocity.
+step :: (Point, Point) -> (Point, Point)
+step (pos, vel@(V2 vx vy)) = (pos + vel, V2 (vx - signum vx) (vy - 1))
+
+-- Fire the probe!
+-- Return a list of all the points it passes through until
+-- it gets past the target.
+fire :: Int -> Int -> Point -> [Point]
+fire xMax yMin = fmap fst . takeWhile ok . iterate step . (zero, )
+  where ok (V2 px py, _) = px <= xMax && py >= yMin
+
+partB :: V2 Point -> Int
+partB target@(V2 (V2 xMin yMin) (V2 xMax yMax)) =
+  length
+    . filter id
+    . fmap (any inTarget)
+    $ [ fire xMax yMin (V2 vx vy)
+      | vx <- [xMinVel .. xMaxVel]
+      , vy <- [yMinVel .. yMaxVel]
+      ]
+ where
+    -- Assumes xMin is positive!
+    -- If xMin is negative this will just bork (sqrt a negative).
+  xMinVel :: Int
+  xMinVel = (floor :: Double -> Int) . sqrt . (* 2) . fromIntegral $ xMin
+  xMaxVel :: Int
+  xMaxVel = xMax
+  -- If yMin is positive, this could be tighter.
+  yMinVel :: Int
+  yMinVel = yMin
+  yMaxVel :: Int
+  yMaxVel = partA target
+
+  inTarget :: Point -> Bool
+  inTarget (V2 x y) = x >= xMin && x <= xMax && y >= yMin && y <= yMax
+
+day17b :: Solution (V2 Point) Int
+day17b = Solution { sParse = parse, sShow = show, sSolve = Right . partB }
