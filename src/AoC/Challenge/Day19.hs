d284570427617bd57d222472ccf20c1bcaf9980a
Matthew Green <mgreen89@gmail.com>
2021-12-19 17:18:38 +0000

    Tidy up day 19 a bit (still very slow)

--- src/AoC/Challenge/Day19.hs
+++ src/AoC/Challenge/Day19.hs
@@ -4,22 +4,20 @@ module AoC.Challenge.Day19
   ) where
 
 import           AoC.Solution
+import           AoC.Util                       ( maybeToEither )
 import           Control.Monad                  ( (<=<)
                                                 , guard
                                                 )
-import           Data.Foldable                  ( find )
-import           Data.List                      ( transpose )
+import           Data.List                      ( tails
+                                                , transpose
+                                                )
 import           Data.List.Split                ( splitOn )
 import           Data.Map                       ( Map )
 import qualified Data.Map                      as M
-import           Data.Maybe                     ( isJust )
 import           Data.Set                       ( Set )
 import qualified Data.Set                      as S
-import           Data.Traversable               ( sequenceA )
 import           Linear.V3                      ( V3(..) )
 import           Text.Read                      ( readEither )
-
-import           Debug.Trace
 
 type Beacon = V3 Int
 type Scanner = V3 Int
@@ -55,7 +53,7 @@ rotY (V3 x y z) = V3 (-z) y x
 rotZ :: Beacon -> Beacon
 rotZ (V3 x y z) = V3 y (-x) z
 
--- Get all the possible orientations.
+-- Get all the possible orientations for a single beacon.
 orientations :: Beacon -> [Beacon]
 orientations = rots <=< initials
  where
@@ -72,106 +70,113 @@ orientations = rots <=< initials
     , (rotZ . rotZ . rotZ $ beacon, rotY)
     ]
 
+-- Get all the possible orientations for beacons from a single scanner.
 scannerOrientations :: Set Beacon -> [Set Beacon]
 scannerOrientations =
   fmap S.fromList . transpose . fmap orientations . S.toList
 
 {-
-   For a pair of scanners, s and s', don't fiddle with s at all and
-   try s' in all 24 oritentations.
+   For a pair of scanners, s0 and s1, don't fiddle with s at all and
+   try s1 in all 24 orientations.
 
-   For each pair of points, p and p', where p is from s and p' is from s',
-   if they are really the same beacon, s' is offset from s by p' - p.
-   Therefore translate all beacons in s' (in the current orientation)
-   by p - p', and check if there are at least 12 overlapping points
-   in s and s'. If so, we've found the relative location of s and s'!
+   For each pair of points, p0 and p1, from s0 and s1 respectively,
+   if they are really the same beacon, s1 is offset from s0 by p1 - p0.
+   Therefore translate all beacons in s1 (in the current orientation)
+   by p0 - p1, and check if there are at least 12 overlapping points
+   in s0 and s1. If so, we've found the relative location of s1!
 
-   Start with scanner 0 as s and try each other scanner until one is found.
-   Then, combining all the scanners with known location into s, try all the
-   scanners with 'unknown' location until an overlap is found.
+   Start with scanner 0 as s0 and try each orientation of each other scanner
+   until one that overlaps is found. Then combine the beacons into the new
+   s0 and continue trying to match the other scanners in the same way.
 
-   Do this until all the scanners have been moved into s, and therefore
-   we know how many beacons there actually are, with their locations
-   relative to scanner 0.
+   Do this until all the scanners have been moved into s0.
 -}
 
 -- | Find the first element in a list that returns a 'Just' value when
---   the operation returns.
+--   applied to the given operation.
 firstJust :: (a -> Maybe b) -> [a] -> Maybe b
-firstJust op []      = Nothing
+firstJust _  []      = Nothing
 firstJust op (a : b) = case op a of
   Just x  -> Just x
   Nothing -> firstJust op b
 
--- Try merging s and s'
--- If a merge was successful, return the new set of placed beacons, and
+-- Try aligning s0 and s1 by checking if each pair could be the same beacon.
+-- If alignment was successful, return the new set of placed beacons, and
 -- the relative position of the matched scanner.
-try :: Set Beacon -> Set Beacon -> Maybe (Set Beacon, Scanner)
-try s s' =
+align :: Set Beacon -> Set Beacon -> Maybe (Set Beacon, Scanner)
+align s0 s1 =
   firstJust id
-    . S.foldr (\p l -> S.foldr (\p' l' -> trySingle p p' : l') l s') []
-    $ s
+    . S.foldr (\p0 l -> S.foldr (\p1 l' -> go p0 p1 : l') l s1) []
+    $ s0
  where
-  trySingle :: Beacon -> Beacon -> Maybe (Set Beacon, Scanner)
-  trySingle p p' = do
-    let delta = p - p'
-        trys' = S.map (+ delta) s'
-    guard $ S.size (S.intersection s trys') >= 12
-    pure (S.union s trys', negate delta)
+  go :: Beacon -> Beacon -> Maybe (Set Beacon, Scanner)
+  go p0 p1 = do
+    let delta = p0 - p1
+        s1'   = S.map (+ delta) s1
+    guard $ S.size (S.intersection s0 s1') >= 12
+    pure (S.union s0 s1', negate delta)
 
-search
+-- Merge one scanner from the set of unplaced scanners into s0, if possible.
+mergeOne
   :: Set Beacon
   -> Map Int Scanner
   -> Map Int (Set Beacon)
   -> Maybe (Set Beacon, Map Int Scanner, Map Int (Set Beacon))
-search s scanners unplaced =
+mergeOne s0 sPoss unplaced =
   firstJust id
-    . foldr attempt []
-    . concatMap (\(k, bs) -> fmap (k, ) (scannerOrientations bs))
+    . foldr attemptMerge []
+    . concatMap (\(k, s1) -> fmap (k, ) (scannerOrientations s1))
     . M.toList
     $ unplaced
  where
-  attempt
+  attemptMerge
     :: (Int, Set Beacon)
     -> [Maybe (Set Beacon, Map Int Scanner, Map Int (Set Beacon))]
     -> [Maybe (Set Beacon, Map Int Scanner, Map Int (Set Beacon))]
-  attempt (s'Id, s') l = case try s s' of
-    Just (newS, scanPos) ->
-      Just (newS, M.insert s'Id scanPos scanners, M.delete s'Id unplaced) : l
+  attemptMerge (s1id, s1) l = case align s0 s1 of
+    Just (s0', sPos) ->
+      Just (s0', M.insert s1id sPos sPoss, M.delete s1id unplaced) : l
     Nothing -> Nothing : l
 
-partARecurse
-  :: Set Beacon -> Map Int Scanner -> Map Int (Set Beacon) -> Maybe (Set Beacon, Map Int Scanner)
-partARecurse bs ss m = case search bs ss m of
-  Just (bs', ss', m') ->
-    if M.size m' == 0 then Just (bs', ss') else partARecurse bs' ss' m'
+mergeScanners
+  :: Set Beacon
+  -> Map Int Scanner
+  -> Map Int (Set Beacon)
+  -> Maybe (Set Beacon, Map Int Scanner)
+mergeScanners s0 scanPos unmerged = case mergeOne s0 scanPos unmerged of
+  Just (s0', scanPos', unmerged') -> if M.null unmerged'
+    then Just (s0', scanPos')
+    else mergeScanners s0' scanPos' unmerged'
   Nothing -> Nothing
 
-partA :: Map Int (Set Beacon) -> Either String Int
-partA beacons =
-  let s        = beacons M.! 0
-      unplaced = M.delete 0 beacons
-      scanners = M.singleton 0 (V3 0 0 0)
-  in  case partARecurse s scanners unplaced of
-        Just (bs, ss) -> Right $ S.size bs
-        Nothing       -> Left "Something went wrong..."
+runMerge :: Map Int (Set Beacon) -> Maybe (Set Beacon, Map Int Scanner)
+runMerge scanners = mergeScanners (scanners M.! 0)
+                                  (M.singleton 0 (V3 0 0 0))
+                                  (M.delete 0 scanners)
 
 manhattan :: V3 Int -> V3 Int -> Int
 manhattan a b = sum . fmap abs $ (b - a)
 
-partB :: Map Int (Set Beacon) -> Either String Int
-partB beacons =
-  let s        = beacons M.! 0
-      unplaced = M.delete 0 beacons
-      scanners = M.singleton 0 (V3 0 0 0)
-  in  case partARecurse s scanners unplaced of
-        Just (bs, ss) -> Right $
-          maximum [ manhattan x y | x <- M.elems ss, y <- M.elems ss ]
-        Nothing -> Left "Something went wrong..."
-
 day19a :: Solution (Map Int (Set Beacon)) Int
-day19a = Solution { sParse = parse, sShow = show, sSolve = partA }
-
+day19a = Solution
+  { sParse = parse
+  , sShow  = show
+  , sSolve = maybeToEither "Unable to merge scanners"
+             . fmap (S.size . fst)
+             . runMerge
+  }
 
 day19b :: Solution (Map Int (Set Beacon)) Int
-day19b = Solution { sParse = parse, sShow = show, sSolve = partB }
+day19b = Solution
+  { sParse = parse
+  , sShow  = show
+  , sSolve = maybeToEither "Unable to merge scanners"
+             . fmap
+                 ( maximum
+                 . (\sps -> [ manhattan x y | (x : ys) <- tails sps, y <- ys ]
+                   )
+                 . M.elems
+                 . snd
+                 )
+             . runMerge
+  }
