6dc523f5fce736d75b171fcfac0aa89778634559
Matthew Green <mgreen89@gmail.com>
2021-12-23 14:08:27 +0000

    Tidy up Day 22

--- src/AoC/Challenge/Day22.hs
+++ src/AoC/Challenge/Day22.hs
@@ -10,20 +10,13 @@ import           Data.Bifunctor                 ( first )
 import           Data.Foldable                  ( asum
                                                 , foldl'
                                                 )
-import           Data.Ix                        ( range )
-import           Data.Maybe                     ( fromJust
-                                                , mapMaybe
-                                                )
-import           Data.Set                       ( Set )
-import qualified Data.Set                      as S
+import           Data.Maybe                     ( mapMaybe )
 import           Data.Void                      ( Void )
 import           GHC.Generics                   ( Generic )
 import           Linear.V3                      ( V3(..) )
 import qualified Text.Megaparsec               as MP
 import qualified Text.Megaparsec.Char          as MP
 import qualified Text.Megaparsec.Char.Lexer    as MPL
-
-import           Debug.Trace
 
 type Cuboid = (V3 Int, V3 Int)
 
@@ -57,43 +50,20 @@ parse :: String -> Either String [Instruction]
 parse =
   first MP.errorBundlePretty . traverse (MP.parse instrParser "day22") . lines
 
-partAMin :: V3 Int
-partAMin = pure (-50)
-
-partAMax :: V3 Int
-partAMax = pure 50
-
-partA :: [Instruction] -> Int
-partA =
-  --S.size . foldl' go S.empty . filter f
-        partB . filter f
- where
-  f :: Instruction -> Bool
-  f Instruction {..} = all (>= -50) iMax && all (<= 50) iMin
-
-  -- Strip any coords we don't care about.
-  toCoords :: Instruction -> [V3 Int]
-  toCoords Instruction {..} =
-    range (max <$> partAMin <*> iMin, min <$> partAMax <*> iMax)
-
-  go :: Set (V3 Int) -> Instruction -> Set (V3 Int)
-  go s i =
-    let iSet = S.fromList . toCoords $ i
-    in  if iOn i then S.union s iSet else S.difference s iSet
-
-day22a :: Solution [Instruction] Int
-day22a = Solution { sParse = parse, sShow = show, sSolve = Right . partA }
-
+-- Find the intersection of two cuboids.
 intersect :: Cuboid -> Cuboid -> Maybe Cuboid
 intersect (aMin, aMax) (bMin, bMax) =
   let (iMin, iMax) = (max <$> aMin <*> bMin, min <$> aMax <*> bMax)
   in  if and (liftA2 (<=) iMin iMax) then Just (iMin, iMax) else Nothing
 
+-- Find the size of a cuboid.
 cuboidSize :: Cuboid -> Int
 cuboidSize (cMin, cMax) = product . fmap ((+ 1) . abs) $ cMax - cMin
 
-partB :: [Instruction] -> Int
-partB = snd . foldl' go ([], 0)
+-- Follow the instructions and return the number of cubes turned on
+-- at the end of the instructions.
+process :: [Instruction] -> Int
+process = snd . foldl' go ([], 0)
  where
   go :: ([(Cuboid, Int)], Int) -> Instruction -> ([(Cuboid, Int)], Int)
   go (cs, n) Instruction {..} =
@@ -107,5 +77,14 @@ partB = snd . foldl' go ([], 0)
   countNodes :: [(Cuboid, Int)] -> Int
   countNodes = sum . fmap (\(c, i) -> i * cuboidSize c)
 
+partAFilter :: Instruction -> Bool
+partAFilter Instruction {..} = all (>= -50) iMax && all (<= 50) iMin
+
+day22a :: Solution [Instruction] Int
+day22a = Solution { sParse = parse
+                  , sShow  = show
+                  , sSolve = Right . process . filter partAFilter
+                  }
+
 day22b :: Solution [Instruction] Int
-day22b = Solution { sParse = parse, sShow = show, sSolve = Right . partB }
+day22b = Solution { sParse = parse, sShow = show, sSolve = Right . process }
