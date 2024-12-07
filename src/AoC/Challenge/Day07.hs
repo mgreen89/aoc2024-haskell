083ddd8f9850cb18178c77a523f3258dec52dc41
Matthew Green <mgreen89@gmail.com>
2024-12-07 10:43:54 +0000

    Day 7a

--- src/AoC/Challenge/Day07.hs
+++ src/AoC/Challenge/Day07.hs
@@ -4,17 +4,51 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module AoC.Challenge.Day07 (
-
+  day07a,
 )
 where
 
--- day07a
 -- , day07b
 
 import AoC.Solution
+import Control.Monad (guard)
+import Data.Bifunctor (first)
+import Data.Foldable (foldl')
+import Data.Functor (($>))
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as NE
+import Data.Maybe (mapMaybe)
+import Data.Void (Void)
+import qualified Text.Megaparsec as MP
+import qualified Text.Megaparsec.Char as MP
+import qualified Text.Megaparsec.Char.Lexer as MPL
 
-day07a :: Solution _ _
-day07a = Solution{sParse = Right, sShow = show, sSolve = Right}
+parser :: MP.Parsec Void String [(Int, NonEmpty Int)]
+parser =
+  MP.sepBy
+    ( do
+        tgt <- MPL.decimal <* MP.char ':' <* MP.hspace1
+        operands <- NE.some1 (MPL.decimal <* MP.hspace)
+        pure (tgt, operands)
+    )
+    MP.newline
+
+parse :: String -> Either String [(Int, NonEmpty Int)]
+parse =
+  first MP.errorBundlePretty . MP.parse parser "day07"
+
+solveA :: [(Int, NonEmpty Int)] -> Int
+solveA =
+  sum . mapMaybe go
+ where
+  go :: (Int, NonEmpty Int) -> Maybe Int
+  go (tgt, x NE.:| xs) = guard (tgt `elem` foldl' go' [x] xs) $> tgt
+
+  go' :: [Int] -> Int -> [Int]
+  go' cs x = [op a x | op <- [(*), (+)], a <- cs]
+
+day07a :: Solution [(Int, NonEmpty Int)] Int
+day07a = Solution{sParse = parse, sShow = show, sSolve = Right . solveA}
 
 day07b :: Solution _ _
 day07b = Solution{sParse = Right, sShow = show, sSolve = Right}
