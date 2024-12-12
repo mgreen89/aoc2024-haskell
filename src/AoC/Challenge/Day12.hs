bbece64e99b1eec500847ed644ab7b7d969e64fd
Matthew Green <mgreen89@gmail.com>
2024-12-12 21:34:17 +0000

    Day 12b

--- src/AoC/Challenge/Day12.hs
+++ src/AoC/Challenge/Day12.hs
@@ -1,24 +1,40 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module AoC.Challenge.Day12 (
   day12a,
+  day12b,
 )
 where
 
---
--- , day12b
-
 import AoC.Common.Point (Dir (..), cardinalNeighbs, dirPoint, parse2dCharMap)
 import AoC.Solution
+import Data.Foldable (foldMap')
 import Data.List (nub)
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Set (Set)
 import qualified Data.Set as S
 import Linear (V2 (..))
+
+findRegions :: Set (V2 Int) -> [Set (V2 Int)]
+findRegions = start []
+  where
+    start :: [Set (V2 Int)] -> Set (V2 Int) -> [Set (V2 Int)]
+    start seen remaining = case S.minView remaining of
+        Nothing -> seen
+        Just (p, ps) ->
+            let (new, remaining') = fill S.empty (S.singleton p) ps
+            in start (new : seen) remaining'
+
+    fill :: Set (V2 Int) -> Set (V2 Int) -> Set (V2 Int) -> (Set (V2 Int), Set (V2 Int))
+    fill old new remaining =
+        if S.null new
+            then (old, remaining)
+            else fill old' new' remaining'
+        where
+            poss = S.difference (foldMap' (S.fromList . cardinalNeighbs) new) old
+            new' = S.intersection poss remaining
+            old' = S.union old new
+            remaining' = S.difference remaining new'
+
 
 findRegion :: (Eq a) => Map (V2 Int) a -> a -> V2 Int -> Set (V2 Int)
 findRegion m x p = go S.empty [p]
@@ -67,5 +83,19 @@ day12a =
     , sSolve = Right . solveA
     }
 
-day12b :: Solution _ _
-day12b = Solution{sParse = Right, sShow = show, sSolve = Right}
+solveB :: Map (V2 Int) Char -> Int
+solveB m =
+  sum
+    [ S.size r * length (findRegions rdns)
+    | r <- toRegions m
+    , d <- [U ..]
+    , let rdns = getRegionDirNeighbs r d
+    ]
+
+day12b :: Solution (Map (V2 Int) Char) Int
+day12b =
+  Solution
+    { sParse = Right . parse2dCharMap
+    , sShow = show
+    , sSolve = Right . solveB
+    }
