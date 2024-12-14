1e042613192373bd87b3d091a14364d9678aa2e9
Matthew Green <mgreen89@gmail.com>
2024-12-14 22:17:43 +0000

    Day 13a

--- src/AoC/Challenge/Day13.hs
+++ src/AoC/Challenge/Day13.hs
@@ -4,50 +4,70 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
 module AoC.Challenge.Day13 (
-day13a,
+  day13a,
 )
 where
 
--- 
+--
 -- , day13b
 
 import AoC.Solution
 import Data.Bifunctor (first)
+import Data.Maybe (mapMaybe)
 import Data.Void (Void)
-import Linear (V2(..))
+import Linear (V2 (..))
 import qualified Text.Megaparsec as MP
 import qualified Text.Megaparsec.Char as MP
 import qualified Text.Megaparsec.Char.Lexer as MPL
 
 parser :: MP.Parsec Void String [(V2 Int, V2 Int, V2 Int)]
 parser =
-    MP.sepBy parseMachine MP.space
-  where
-    parseMachine = do
-        MP.string "Button A: X+"
-        ax <- MPL.decimal
-        MP.string ", Y+"
-        ay <- MPL.decimal
-        MP.newline
-        MP.string "Button B: X+"
-        bx <- MPL.decimal
-        MP.string ", Y+"
-        by <- MPL.decimal
-        MP.newline
-        MP.string "Prize: X="
-        px <- MPL.decimal
-        MP.string ", Y="
-        py <- MPL.decimal
-        MP.newline
-        pure (V2 ax ay, V2 bx by, V2 px py)
-
+  MP.sepBy parseMachine MP.space
+ where
+  parseMachine = do
+    MP.string "Button A: X+"
+    ax <- MPL.decimal
+    MP.string ", Y+"
+    ay <- MPL.decimal
+    MP.newline
+    MP.string "Button B: X+"
+    bx <- MPL.decimal
+    MP.string ", Y+"
+    by <- MPL.decimal
+    MP.newline
+    MP.string "Prize: X="
+    px <- MPL.decimal
+    MP.string ", Y="
+    py <- MPL.decimal
+    pure (V2 ax ay, V2 bx by, V2 px py)
 
 parse :: String -> Either String [(V2 Int, V2 Int, V2 Int)]
 parse =
   first MP.errorBundlePretty . MP.parse parser "day13"
 
-day13a :: Solution _ _
-day13a = Solution{sParse = Right, sShow = show, sSolve = Right}
+-- | Solve the simeltaneous equations and return (if possible) and integer solution.
+{- Slightly simplified initial example to help work out.
+94a + 22b = 840
+34a + 67b = 540
+a = (540 - 67b) / 34
+94 * 540 / 34 - 94 * 67b / 34 + 22b = 840
+94 * (540 - 67b) + (22 * 34)b = 840 * 34
+((22 * 34) - (94 * 67))b = (840 * 34) - (94 * 540)
+-5550b = -2220
+b = 4
+-}
+solveSimel :: (V2 Int, V2 Int, V2 Int) -> Maybe (Int, Int)
+solveSimel (V2 ax ay, V2 bx by, V2 tx ty) =
+  let (b, bRem) = (tx * ay - ty * ax) `divMod` (ay * bx - ax * by)
+   in if bRem == 0 then Just ((ty - (by * b)) `div` ay, b) else Nothing
+
+day13a :: Solution [(V2 Int, V2 Int, V2 Int)] Int
+day13a =
+  Solution
+    { sParse = parse
+    , sShow = show
+    , sSolve = Right . sum . fmap (\(a, b) -> 3 * a + b) . mapMaybe solveSimel
+    }
 
 day13b :: Solution _ _
 day13b = Solution{sParse = Right, sShow = show, sSolve = Right}
