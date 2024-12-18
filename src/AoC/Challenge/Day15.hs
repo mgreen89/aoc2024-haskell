9272a6d98a953cc4b04300385843249196d2240c
Matthew Green <mgreen89@gmail.com>
2024-12-18 19:29:59 +0000

    Day 15b

--- src/AoC/Challenge/Day15.hs
+++ src/AoC/Challenge/Day15.hs
@@ -12,7 +12,6 @@ import Data.Foldable (foldMap', foldl')
 import Data.List.Split (splitOn)
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Maybe (fromMaybe)
 import Data.Set (Set)
 import qualified Data.Set as S
 import Linear (V2 (..), (*^))
@@ -54,67 +53,68 @@ solveA ((walls, boxes, robot), insts) =
 day15a :: Solution ((Set (V2 Int), Set (V2 Int), V2 Int), [Dir]) Int
 day15a = Solution{sParse = parse, sShow = show, sSolve = Right . solveA}
 
-gps' :: Map (V2 Int) (V2 Int) -> V2 Int -> Int
-gps' bs b@(V2 x _) =
-  case bs M.!? b of
-    Just (V2 x' _) | x' == x + 1 -> gps b
-    _ -> 0
-
-stepB :: Set (V2 Int) -> (Map (V2 Int) (V2 Int), V2 Int) -> Dir -> (Map (V2 Int) (V2 Int), V2 Int)
+stepB :: Set (V2 Int) -> (Map (V2 Int) Bool, V2 Int) -> Dir -> (Map (V2 Int) Bool, V2 Int)
 stepB walls (boxes, robot) dir =
-  fromMaybe
-    (boxes, robot)
-    ( case dir of
-        U -> goV [] [robot'] 0
-        -- R -> goR [] [robot'] 0
-        -- D -> goV [] [robot'] 0
-        -- L -> goL [] [robot'] 0)
-    )
+  case maybeMove S.empty (S.singleton robot') of
+    Just moved ->
+      let toMove = M.restrictKeys boxes moved
+          new = M.fromList . fmap (\(p, b) -> (p + dirPoint dir, b)) $ M.toList toMove
+       in (M.union new (M.withoutKeys boxes moved), robot')
+    Nothing -> (boxes, robot)
  where
+  robot' :: V2 Int
   robot' = robot + dirPoint dir
-  goV :: [V2 Int] -> [V2 Int] -> Int -> Maybe (Map (V2 Int) (V2 Int), V2 Int)
-  goV old new ofst = Nothing
 
-{-
- for b in bs:
-   if b is empty, tick.
-   if b is wall, fail.
-   if b is box, add both box things.
--}
-{-let old' = old ++ new
-    p = robot' + (ofst *^ dirPoint dir)
- in if any (`S.member` walls) new
-      then Nothing
-      else concatMap (\p@(V2 x y) -> if M.member p boxes then [] else [])
-        if S.member p boxes
-          then go (nbs + 1)
-          else
-            if nbs == 0
-              then (boxes, robot')
-              else (S.insert p . S.delete robot' $ boxes, robot')-}
+  boxesKeys = M.keysSet boxes
+
+  -- Returns the set of boxes to move if successful.
+  maybeMove :: Set (V2 Int) -> Set (V2 Int) -> Maybe (Set (V2 Int))
+  maybeMove old new
+    | not (S.null (S.intersection walls new)) = Nothing
+    | S.null (S.intersection new boxesKeys) = Just old
+    | otherwise = maybeMove old' new'
+   where
+    expanded =
+      S.fromList
+        [ p
+        | n <- S.toList (S.intersection boxesKeys new)
+        , p <-
+            if boxes M.!? n == Just True
+              then [n, n + V2 1 0]
+              else [n - V2 1 0, n]
+        ]
+    old' = old <> expanded
+    new' = S.difference (S.map (+ dirPoint dir) expanded) expanded
 
 solveB :: ((Set (V2 Int), Set (V2 Int), V2 Int), [Dir]) -> Int
 solveB ((walls, boxes, robot), insts) =
   let
+    walls' :: Set (V2 Int)
     walls' =
       foldMap'
         ( \(V2 x y) ->
             S.fromList [V2 (2 * x) y, V2 (2 * x + 1) y]
         )
         walls
+
+    boxes' :: Map (V2 Int) Bool
     boxes' =
       foldMap'
         ( \(V2 x y) ->
             M.fromList
-              [ (V2 (2 * x) y, V2 (2 * x + 1) y)
-              , (V2 (2 * x + 1) y, V2 (2 * x) y)
+              [ (V2 (2 * x) y, True)
+              , (V2 (2 * x + 1) y, False)
               ]
         )
         boxes
+
+    robot' :: V2 Int
     robot' = (\(V2 x y) -> V2 (2 * x) y) robot
    in
     sum
-      . (\bs -> fmap (gps' bs) (M.keys bs))
+      . fmap gps
+      . M.keys
+      . M.filter id
       . fst
       . foldl' (stepB walls') (boxes', robot')
       $ insts
